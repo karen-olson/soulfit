63fd1b16ecf911d2f340727866885173aa8b5d62
Karen Olson <karmarie@gmail.com>
2022-01-24 11:17:56 -0600

    Add logout functionality on backend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorize, only: :create
+    skip_before_action :authorize
     
     def create 
         user = User.find_by(username: params[:username])
@@ -11,5 +11,9 @@ class SessionsController < ApplicationController
         end
     end
 
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
   resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
