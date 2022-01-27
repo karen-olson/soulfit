25aa9aabd496ad2e0ed56ae32f7e59627948bd4c
Karen Olson <karmarie@gmail.com>
2022-01-26 19:10:32 -0600

    Write CRUD actions for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,39 @@
 class CategoriesController < ApplicationController
 
+    def index 
+        categories = Category.all
+        render json: categories
+    end
+
+    def show
+        category = find_category
+        render json: category
+    end
+
+    def create
+        category = Category.create!(category_params)
+        render json: category, status: :created
+    end
+
+    def update
+        category = find_category
+        category.update!(category_params)
+        render json: category, status: :accepted
+    end
+
+    def destroy
+        category = find_category
+        category.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_category
+        Category.find(params[:id])
+    end
+
+    def category_params
+        params.permit(:id, :name)
+    end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
     has_many :videos
+
+    validates :name, presence: true, uniqueness: true
 end
