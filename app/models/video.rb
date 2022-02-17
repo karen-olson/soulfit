44a812f6934310b668a07392769412edc45df79a
Karen Olson <karmarie@gmail.com>
2022-02-17 16:15:36 -0600

    Destroy all dependent records when a video is destroyed.

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,5 +4,8 @@ class Video < ApplicationRecord
   has_one :user_added_video, dependent: :destroy
   has_one :video_added_by_user, through: :user_added_video
 
+  has_one :user_saved_video, dependent: :destroy
+  has_one :video_saved_by_user, through: :user_saved_video
+
   validates :url, :title, :channel_title, :duration, :youtube_video_id, :category_id, presence: true
 end
